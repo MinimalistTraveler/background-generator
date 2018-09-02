c21b9efdbabe49d752d1df0c0750d645a3a77734
Erik <erikzb7@gmail.com>
2018-09-02 19:30:59 -0400

    version 2.0 changes

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,47 @@
 const $ = query => document.querySelector(query);
 const $all = query => document.querySelectorAll(query);
 
-const colorUpdate = (color1, color2) => {
+const colorUpdate = (color1, color2, dir = "right") => {
+  let colorContent = $("#color-content");
   $(
     "body"
-  ).style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  ).style.background = `linear-gradient(to ${dir}, ${color1}, ${color2})`;
+  colorContent.textContent = `background: linear-gradient(to ${dir},${color1}, ${color2});`;
 };
-const updateText = (content, color1, color2) => {
-  content.textContent = `Color 1: ${color1}, Color 2: ${color2}`;
-};
+
 $(".color-pickers").addEventListener("input", e => {
   if (e.target.classList.contains("color-item")) {
     let colors = $all(".color-item");
     let color1 = colors[0].value;
     let color2 = colors[1].value;
-    let colorContent = $("#color-content");
+
     // Update the colors
     colorUpdate(color1, color2);
-    updateText(colorContent, color1, color2);
   }
 });
-
+document.addEventListener("keypress", e => {
+  let colors = $all(".color-item");
+  let color1 = colors[0].value;
+  let color2 = colors[1].value;
+  const keyCodes = {
+    top: 38,
+    bottom: 40,
+    left: 37,
+    right: 39
+  };
+  const targetKey = Object.values(keyCodes).find(key => key === e.keyCode);
+  if (!targetKey) {
+    return;
+  }
+  // Check if the keyCodes value match the target value
+  const targetKeyArr = Object.entries(keyCodes).find(
+    key => targetKey === key[1]
+  );
+  colorUpdate(color1, color2, targetKeyArr[0]);
+});
 document.addEventListener("DOMContentLoaded", () => {
+  let colors = $all(".color-item");
   colorUpdate("#DDD", "#FFF");
+  colors[0].value = "";
+  colors[1].value = "";
 });
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# Background Generator Version 1 Aug 2018
+# Background Generator Version 2.0 Aug 2018
 
 Recent Changes So Far:
 
+## Version 1: Changes
+
 - Background Changes When Color Is Selected.
+
+## Version 2: Changes
+
+- Change text to the css background property for convience.
+- Using the keyboard 'left, right, up, down' keys updates the background
+  to the direction of the keys. For instance pressing up on the keyboard
+  moves the linear gradient to the top.
